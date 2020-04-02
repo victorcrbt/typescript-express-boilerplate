83d3d4e9111680d6cd26514ec4190b58e915eac9
Victor Batalha <madivcb@gmail.com>
2020-04-02 02:18:07 -0300

    changed truncate function

--- __tests__/util/truncate.ts
+++ __tests__/util/truncate.ts
@@ -1,52 +1,58 @@
 import { Connection } from 'typeorm';
 import database from '../../src/database';
 
+import knex from '../../src/database/knex';
+
 export default function truncate(connection?: Connection) {
   if (connection) {
     return Promise.all(
-      connection.entityMetadatas.map((entity) => {
+      connection.entityMetadatas.map(async (entity) => {
         const { name, tableName } = entity;
 
-        connection.getRepository(name).clear();
+        await connection.getRepository(name).clear();
 
-        if (connection.options.type === 'sqlite') {
-          connection.query(
-            `UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='${tableName}'`
-          );
-        } else if (connection.options.type === 'postgres') {
-          connection.query(`ALTER SEQUENCE ${tableName}_id_seq RESTART WITH 1`);
-        } else if (
-          connection.options.type === 'mysql' ||
-          connection.options.type === 'mariadb'
-        ) {
-          connection.query(`ALTER TABLE ${tableName} AUTO_INCREMENT = 1`);
-        }
+        await knex(tableName).truncate();
+
+        // if (connection.options.type === 'sqlite') {
+        //   connection.query(
+        //     `UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='${tableName}'`
+        //   );
+        // } else if (connection.options.type === 'postgres') {
+        //   connection.query(`ALTER SEQUENCE ${tableName}_id_seq RESTART WITH 1`);
+        // } else if (
+        //   connection.options.type === 'mysql' ||
+        //   connection.options.type === 'mariadb'
+        // ) {
+        //   connection.query(`ALTER TABLE ${tableName} AUTO_INCREMENT = 1`);
+        // }
       })
     );
   }
 
   return Promise.all(
-    database.connection.entityMetadatas.map((entity) => {
+    database.connection.entityMetadatas.map(async (entity) => {
       const { name, tableName } = entity;
 
-      database.connection.getRepository(name).clear();
+      await database.connection.getRepository(name).clear();
 
-      if (database.connection.options.type === 'sqlite') {
-        database.connection.query(
-          `UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='${tableName}'`
-        );
-      } else if (database.connection.options.type === 'postgres') {
-        database.connection.query(
-          `ALTER SEQUENCE ${tableName}_id_seq RESTART WITH 1`
-        );
-      } else if (
-        database.connection.options.type === 'mysql' ||
-        database.connection.options.type === 'mariadb'
-      ) {
-        database.connection.query(
-          `ALTER TABLE ${tableName} AUTO_INCREMENT = 1`
-        );
-      }
+      await knex(tableName).truncate();
+
+      // if (database.connection.options.type === 'sqlite') {
+      //   database.connection.query(
+      //     `UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='${tableName}'`
+      //   );
+      // } else if (database.connection.options.type === 'postgres') {
+      //   database.connection.query(
+      //     `ALTER SEQUENCE ${tableName}_id_seq RESTART WITH 1`
+      //   );
+      // } else if (
+      //   database.connection.options.type === 'mysql' ||
+      //   database.connection.options.type === 'mariadb'
+      // ) {
+      //   database.connection.query(
+      //     `ALTER TABLE ${tableName} AUTO_INCREMENT = 1`
+      //   );
+      // }
     })
   );
 }
