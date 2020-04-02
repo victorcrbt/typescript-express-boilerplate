264150fc78243137efb54cf26b65825e5a6437e7
Victor Batalha <madivcb@gmail.com>
2020-04-02 02:00:35 -0300

    removed controller

--- src/app/controllers/TestController.ts
+++
@@ -1,8 +0,0 @@
-import { ControllerMethod } from 'express';
-
-class TestController {
-  public index: ControllerMethod = async (req, res) =>
-    res.send('Hello, ESLint!');
-}
-
-export default new TestController();
